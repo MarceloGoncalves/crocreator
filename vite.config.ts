bf74be6dfb8b37ab09c0725775d27b245823d437
Vitoria <marcelogoncalvescdd@gmail.com>
2026-06-12 17:48:06 -0300

    fix: add sobre.html to vite build entry points

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     port: 5173
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sobre: resolve(__dirname, 'sobre.html')
+      }
+    }
   }
 })
